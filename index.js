5d2de68fed01f3c9708d296ebe02954f4ed5250e
Jakob Lindskog <jakob.lindskog@gmail.com>
2018-02-28 16:29:45 +0100

    bug fix for reactions

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const fs = require('fs')
 const path = require('path')
 
+const ProgressBar = require('progress')
+
 let msg = require('./lib/msg')
 
 try { fs.mkdirSync('output') }
-catch(e){
+catch(e){ }
 
-}
+function iterateAll({complete = true}){
+    const alreadyDone = fs.readdirSync('output').map(t=>t.replace('json','html'))
+    const files = fs.readdirSync('messages').filter(t=> path.extname(t)==='.html' && (complete || alreadyDone.indexOf(t)===-1)).sort((a,b)=>parseInt(a.split('.')[0]) - parseInt(b.split('.')[0]))
 
-function iterateAll(){
-    const files = fs.readdirSync('messages')
+    const bar = new ProgressBar(':bar :currentFile', {total: files.length})
 
-    files.filter(t=>t.indexOf('/')===-1).forEach(t=>{
+    files.forEach(t=>{
+        console.log('curr file ' + t)
         const f = path.join('messages', t)
         const out = path.join('output', t.replace('html','json'))
         const m = JSON.stringify(msg(f, 'sv'), null, 2)
         fs.writeFileSync(out, m)
+        bar.tick({currentFile: f})
     })
 }
 
-// iterateAll()
+iterateAll(false)
 
-let res = msg('messages/101.html', 'sv')
-fs.writeFileSync('out.json', JSON.stringify(res, null, 2))
+// let res = msg('messages/100.html', 'sv')
+// fs.writeFileSync('out.json', JSON.stringify(res, null, 2))
