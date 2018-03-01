f725a602af278a2a8a4f68c250ada328bc205ada
Jakob Lindskog <jakob.lindskog@gmail.com>
2018-03-01 11:42:56 +0100

    file for quickly testing boolean logic

---
+++ logic.js
@@ -0,0 +1,28 @@
+/*
+    if overwrite:
+        don't take into consideration what alreadyDone gives (ie keep all files, filter 1)
+    else
+        take into consideration what alreadyDone gives
+    
+    if overwrite:
+        filter(t=>true)
+    else
+        filter(t=>alreadyDone)
+
+    0:0 => 1
+    0:1 => 0
+    1:0 => 1
+    1:1 => 1
+
+    0:1 => 0
+    0:0 => 1
+    1:- => 1
+
+    !((a^b)*!a)
+*/
+// const t = (a,b)=>!((a^b)&&!a)
+const t = (a,b)=>!(a^b)||a
+
+for(var i = 0; i <= 1; i++)
+    for(var j = 0; j <= 1; j++)
+        console.log(`${i}:${j} => ${Number(t(i,j))}`)
